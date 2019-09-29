085cf968ca45b109a4d29c21462a31b781d21a7b
LisaCee <lisaceedesign@gmail.com>
2019-09-29 14:03:39 -0700

    getting characters with hooks

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,23 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  const [characters, getCharacters] = useState([]);
+  
   useEffect(() => {
+    
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+  axios.get(`https://rickandmortyapi.com/api/character/`) 
+    .then(res => {getCharacters(res.data.results)})
+    .catch(err => console.log(err))
   }, []);
-
+  console.log(characters)
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map((character) => 
+        <li key={character.id}>{character.name}</li>)}
     </section>
   );
 }
